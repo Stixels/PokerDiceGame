97306b1cb70581aa6872db986b8c03a8f6c88ea6
Matthew Mercado <89710831+Stixels@users.noreply.github.com>
2022-02-27 20:00:00 -0500

    Info Accordions

--- pokerDice.js
+++ pokerDice.js
@@ -1,1 +1,17 @@
 // All js for poker dice game
+
+// JS for accordions
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
